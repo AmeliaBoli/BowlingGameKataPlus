7c569103dfeff4d1e3f33b0e9fe0884cb5b30611
admim <admim@admims-MacBook-Pro.local>
2019-03-17 17:53:06 -0700

    Bowling Game Kata Final State

--- BowlingGameTests/BowlingGameTests.swift
+++ BowlingGameTests/BowlingGameTests.swift
@@ -10,25 +10,60 @@ import XCTest
 @testable import BowlingGame
 
 class BowlingGameTests: XCTestCase {
-
+    var game: Game!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        game = Game()
     }
-
+    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        game = nil
+        super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    private func rollMany(pins: Int, times: Int) {
+        for _ in 1...times {
+            game.roll(pins)
         }
     }
-
+    
+    func testGutterGame() {
+        rollMany(pins: 0, times: 20)
+        XCTAssertEqual(game.score(), 0)
+    }
+    
+    func testAllOnes() {
+        rollMany(pins: 1, times: 20)
+        XCTAssertEqual(game.score(), 20)
+    }
+    
+    private func rollSpare() {
+        game.roll(5)
+        game.roll(5)
+    }
+    
+    private func rollStrike() {
+        game.roll(10)
+    }
+    
+    func testOneSpare() {
+        rollSpare()
+        game.roll(3)
+        rollMany(pins: 0, times: 17)
+        XCTAssertEqual(game.score(), 16)
+    }
+    
+    func testOneStrike() {
+        rollStrike()
+        game.roll(3)
+        game.roll(4)
+        rollMany(pins: 0, times: 16)
+        XCTAssertEqual(game.score(), 24)
+    }
+    
+    func testPerfectGame() {
+        rollMany(pins: 10, times: 12)
+        XCTAssertEqual(game.score(), 300)
+    }
 }
